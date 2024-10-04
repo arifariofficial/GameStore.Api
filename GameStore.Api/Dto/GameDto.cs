c12a299f1e4360b64cf696f5e08106fd9216f684
Ariful Islam <ariful6@yahoo.com>
2024-10-04 20:37:46 +0300

    all games api

--- GameStore.Api/Dto/GameDto.cs
+++ GameStore.Api/Dto/GameDto.cs
@@ -1,6 +1,3 @@
 namespace GameStore.Api.Dto;
 
-public record class GameDto
-{
-
-}
+public record class GameDto(int Id, string Name, string Genre, decimal Price, DateOnly ReleaseDate);
--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,32 @@
+using GameStore.Api.Dto;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<GameDto> games = [
+    new(
+        1,
+        "Street Fighter 2",
+        "Fighting",
+        19.99M,
+        new DateOnly(2000, 12, 31)),
+    new (
+        2,
+        "Final Fantasy XIV",
+        "Roleplaying",
+        59.99M,
+        new DateOnly(2010, 9, 30)),
+    new (
+        3,
+        "FIFA 23",
+        "Sports",
+        69.99M,
+        new DateOnly(2022, 9, 27)),
+];
+
+
+// GET /games
+app.MapGet("/games", () => games);
+
 
 app.Run();
