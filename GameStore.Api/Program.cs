2d5c92efd4cd904561f4ca9caf9c7a1ae32fac9c
Ariful Islam <ariful6@yahoo.com>
2024-10-04 21:48:58 +0300

    create game

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -3,6 +3,8 @@ using GameStore.Api.Dto;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+const string GetGameEndpointName = "GetGameById";
+
 List<GameDto> games = [
     new(
         1,
@@ -28,5 +30,23 @@ List<GameDto> games = [
 // GET /games
 app.MapGet("/games", () => games);
 
+// GET /games/{id}
+app.MapGet("games/{id}", (int id) => games.Find(g => g.Id == id))
+    .WithName(GetGameEndpointName);
+
+// POST /games
+app.MapPost("/games", (CreateGameDto createGameDto) =>
+{
+    GameDto newGame = new(
+        games.Max(g => g.Id) + 1,
+        createGameDto.Name,
+        createGameDto.Genre,
+        createGameDto.Price,
+        createGameDto.ReleaseDate
+    );
+    games.Add(newGame);
+
+    return Results.CreatedAtRoute(GetGameEndpointName, new { id = newGame.Id }, newGame);
+});
 
 app.Run();
